abda2e02a1fd4575c84d8cd40408dbc9edf1a1e2
imtiazOvi <imtiazuddin018@gmail.com>
2020-03-16 01:14:00 +0600

    mutableSetOf added

--- app/src/main/java/com/example/kotlin_learning_part1/collections/MutableSetOf.kt
+++ app/src/main/java/com/example/kotlin_learning_part1/collections/MutableSetOf.kt
@@ -2,14 +2,18 @@ package com.example.kotlin_learning_part1.collections
 
 fun main(){
 
+
     // "Set" contains unique elements
     // "HashSet" also contain unique elements, but the sequence not guaranteed in output
 
-    var mySet = setOf<Int>(2,4,56,7,78,9)  // Immutable, read only
+    var mySet = mutableSetOf<Int>(2,4,56,7,78,9)  // mutable set, read and write both
+
+    mySet.remove(4)
+    mySet.add(200)
 
 
     // in myMap.keys have all of keys like 2,43,7
     for (elements in mySet){ //using individual element (object)
-        println(elements)   
+        println(elements)
     }
 }
