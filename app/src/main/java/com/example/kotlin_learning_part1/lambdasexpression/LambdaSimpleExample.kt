4616fbb4852127b7a556b01f2fe2e8faf4579339
imtiazOvi <imtiazuddin018@gmail.com>
2020-03-15 14:41:12 +0600

    Simple lambda expression example

---
+++ app/src/main/java/com/example/kotlin_learning_part1/lambdasexpression/LambdaSimpleExample.kt
@@ -0,0 +1,16 @@
+package com.example.kotlin_learning_part1.lambdasexpression
+
+fun main(){
+
+    var myLambdaObj = MyLambda()
+
+    myLambdaObj.addTwoNumber(2,3,{x,y -> x + y}) // lambda function
+    // myLambdaObj.addTwoNumber(2,3){x,y -> x + y} // another way of lambda function
+
+}
+
+class MyLambda{
+    fun addTwoNumber(number1: Int, number2: Int, action: (Int , Int) -> Int){
+        println("Sum is: ${action(number1,number2)}")
+    }
+}
